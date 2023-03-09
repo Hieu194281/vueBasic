2d5e56e0637b7095bb7c9261aeaecfc145a9b7f6
Hieu194281 <trunghieu2208@gmail.com>
2023-03-09 15:14:19 +0700

    first-commit

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <input type="text" v-model="channel.name" />
+    <div>{{ name }}</div>
+
+    <button @click="isShow = !isShow">
+      <span v-if="isShow">An</span>
+      <span v-else>Hien</span>
+    </button>
+    <h1 v-bind:id="id" v-show="isShow" :class="{ dachon: selected }"> Hieu IT TV Show</h1>
+    <button @click="selected = !selected">Click</button>
+    <button v-on:click="channel.name = 'ABC'">Change name</button>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+// import MyMenuMain from './components/MyMenuMain.vue';
+
+
+export default {
+  data() {
+    return {
+      channel: {
+        name: 'Lap trinh abc'
+      },
+      id: 'abc',
+      isShow: true,
+      selected: true
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.dachon {
+  color: red;
+}
+</style>
---
+++ src/components/MyMenuMain.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h2>Daday la menu</h2>
+        Tao la hieu day
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Mymenumain'
+}
+</script>
+
+<style></style>
